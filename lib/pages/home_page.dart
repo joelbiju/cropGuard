ad0e36dd420f62f42fdf909462d04bef6ffe309f
joelbiju <joelbiju547@gmail.com>
2024-02-02 12:29:55 +0530

    homepage initialized

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@
 
 import 'dart:ffi';
 
+import 'package:crop_guard/pages/disease_page.dart';
+import 'package:crop_guard/util/home_card.dart';
 import 'package:flutter/material.dart';
 
 class MyHome extends StatelessWidget {
@@ -32,8 +34,10 @@ class MyHome extends StatelessWidget {
 
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-
+        
             //welcome note
             Row(
               children: [
@@ -53,12 +57,79 @@ class MyHome extends StatelessWidget {
                 ),
               ],
             ),
-
+        
             SizedBox(height: 20,),
-
+        
             //carousel
-            
-
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Various Plant Diseases',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 18
+                    ),),
+                    GestureDetector(
+                        onTap: () {
+                        // Navigate to another page
+                        Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Diseases()),
+                    );
+                  },
+                    child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'see all',
+                      style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                ),
+            ),
+                  ],
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      HomeCard(
+                        cardNumber: '01', 
+                        cardTitle: 'Rice Blast', 
+                        cardDesc: "The rice blast disease, which is caused by a fungus Mangnaporthe oryzae known as one of the most formidable and economically impactful maladies affecting rice agriculture globally. With rice serving as a primary dietary staple for more than half of the world's population, this disease carries significant consequences."
+                        ),
+                      HomeCard(
+                        cardNumber: '02', 
+                        cardTitle: 'Bacterial Blight', 
+                        cardDesc: "Bacterial blight, a concerning issue in rice cultivation, is attributed to the presence of the Xanthomonas oryzae, primarily affecting regions with warm and humid climates. It presents unique and easily recognizable visual indications."
+                        ),
+                      HomeCard(
+                        cardNumber: '03', 
+                        cardTitle: 'Brown Spot', 
+                        cardDesc: "Brown spots on rice plants are a common symptom of various plant diseases. These spots typically appear as small to large discolored areas on the leaves, stems, or grains of the rice plant."
+                        ),
+                      HomeCard(
+                        cardNumber: '04', 
+                        cardTitle: 'Leaf Smut', 
+                        cardDesc: "Leaf smut, in rice plants is the result of an infection by the Ustilaginoidea virens fungus. The disease is marked by the appearance of small, initially green to whitish spore balls on rice panicles, which subsequently change into smoky gray to dark green or black formations."
+                        ),
+                      HomeCard(
+                        cardNumber: '05', 
+                        cardTitle: 'Tungro', 
+                        cardDesc: "Tungro is a viral disease of rice that is transmitted by leafhoppers. It is caused by two viruses: rice tungro spherical virus (RTSV) and rice tungro bacilliform virus (RTBV). This viral infection manifests in rice plants as leaf discoloration (yellowing or reddening), stunted growth leading to significant yield losses."
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+        
+            /////
+        
           ],
         ), 
         ),
---
+++ lib/util/home_card.dart
@@ -0,0 +1,93 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:crop_guard/pages/disease_page.dart';
+import 'package:flutter/material.dart';
+
+class HomeCard extends StatelessWidget {
+
+  final String cardNumber;
+  final String cardTitle;
+  final String cardDesc;
+
+  const HomeCard({super.key, 
+  required this.cardNumber, 
+  required this.cardTitle, 
+  required this.cardDesc});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+                height: 230,
+                width: MediaQuery.of(context).size.width,
+                child: Card(
+                  color: Color.fromRGBO(41, 166, 148, 0.5),
+                  surfaceTintColor: Colors.transparent,
+                  elevation: 0,
+                  
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                  // margin: EdgeInsets.all(15),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+                    
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 5,),
+                        //card number
+                        Text(cardNumber,
+                        style: TextStyle(
+                          fontSize: 20, 
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white)),
+
+                        SizedBox(height: 10,),
+                    
+                        //card title
+                        Text(cardTitle,
+                        style: TextStyle(
+                          fontSize: 20, 
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                    
+                        SizedBox(height: 2,),
+                    
+                        //card description
+                        Text(cardDesc,
+                        textAlign: TextAlign.justify,
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 18, 
+                          fontWeight: FontWeight.w300,
+                          color: Colors.white)),
+
+                          GestureDetector(
+                        onTap: () {
+                        // Navigate to another page
+                        Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Diseases()),
+                    );
+                  },
+                    child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'more',
+                      style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                ),
+            ),
+                
+                        SizedBox(height: 10,),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+  }
+}
